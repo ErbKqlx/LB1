adf0c557dd2b72a5211c640fe78831eefd09017c
Erbeen <Error5455@yandex.ru>
2024-11-21 14:58:48 +0500

    Добавление связи с БД используя метод EnsureCreated, вывод списка пользователей

---
+++ LB1/FormMain.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace LB1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelTop = new Panel();
+            labelUsers = new Label();
+            panel1 = new Panel();
+            labelInfAboutUsers = new Label();
+            panelTop.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(labelUsers);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(0, 0);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(10);
+            panelTop.Size = new Size(684, 70);
+            panelTop.TabIndex = 0;
+            // 
+            // labelUsers
+            // 
+            labelUsers.Dock = DockStyle.Fill;
+            labelUsers.Location = new Point(10, 10);
+            labelUsers.Name = "labelUsers";
+            labelUsers.Size = new Size(664, 50);
+            labelUsers.TabIndex = 0;
+            labelUsers.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(labelInfAboutUsers);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 70);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(10);
+            panel1.Size = new Size(684, 491);
+            panel1.TabIndex = 1;
+            // 
+            // labelInfAboutUsers
+            // 
+            labelInfAboutUsers.Dock = DockStyle.Fill;
+            labelInfAboutUsers.Location = new Point(10, 10);
+            labelInfAboutUsers.Name = "labelInfAboutUsers";
+            labelInfAboutUsers.Size = new Size(664, 471);
+            labelInfAboutUsers.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(684, 561);
+            Controls.Add(panel1);
+            Controls.Add(panelTop);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(5);
+            Name = "FormMain";
+            Text = "Пользователи";
+            Load += FormMain_Load;
+            panelTop.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelTop;
+        private Panel panel1;
+        private Label labelUsers;
+        private Label labelInfAboutUsers;
+    }
+}
